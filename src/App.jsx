b4a79fb76535e35555f3ef3b1f62e1dfafeaa2ca
Marina Escriva <marinaescriva.24@gmail.com>
2024-05-04 19:54:08 +0200

    header is in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css'
-import { Body } from './pages/Body/Body'
+import { Body } from './pages/Body/Body';
+import { Header } from './common/Header/Header';
 
 function App() {
   return (
     <>
-     <Body/>
+    <Header/>
+    <Body/>
     </>
   )
 }
---
+++ src/common/Header/Header.jsx
@@ -0,0 +1,8 @@
+import "./Header.css";
+export const Header = () => {
+  return (
+    <div className="headerDesign">
+
+    </div>
+  );
+}
