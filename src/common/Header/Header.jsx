64cbd708f650b54a61decf2792e4ce2d627d7d3f
Marina Escriva <marinaescriva.24@gmail.com>
2024-05-06 22:10:03 +0200

    update: username and superadmin profile views added on header, good reading properties from rdx credenciales

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -5,9 +5,9 @@ import { userData } from "../../app/slices/userSlice";
 export const Header = () => {
 
     const rdxUser = useSelector(userData);
- console.log(rdxUser , "rdxUser")
- console.log(rdxUser.credenciales.token , "token")
-   console.log(rdxUser.credenciales.user.id_role , "id_role")
+    console.log(rdxUser, "rdxUser")
+    console.log(rdxUser.credenciales.token, "token")
+    console.log(rdxUser.credenciales.user.id_role, "id_role")
 
     return (
         <div className="headerDesign">
@@ -16,20 +16,29 @@ export const Header = () => {
             />
 
             {
-                rdxUser.credenciales.token
+                rdxUser?.credenciales?.token
                     ? (<div>
 
                         <Navigator title={rdxUser.credenciales.user.nombre} destination={"/profile"} />
-                        <Navigator title={"LOG OUT"} onClick ={() => logOut()} />
+                        <Navigator title={"reservar"} destination={"/"} />
+
+                        {(rdxUser.credenciales.user.id_role === 1)
+                            ? <Navigator path="/admin" title="Super Admin" />
+                            : null
+                        }
+
+                        <div onClick={() => dispatch(logout({ credenciales: "" }))}>
+                            <Navigator path="/" title={"Log out"} />
+                        </div>
 
                     </div>
                     ) : (
-                    <div className="authMenu">
+                        <div className="authMenu">
 
-                        <Navigator title={"REGISTER"} destination={"/register"} />
-                        <Navigator title={"LOGIN"} destination={"/login"} />
+                            <Navigator title={"REGISTER"} destination={"/register"} />
+                            <Navigator title={"LOGIN"} destination={"/login"} />
 
-                    </div>)
+                        </div>)
             }
 
         </div>
